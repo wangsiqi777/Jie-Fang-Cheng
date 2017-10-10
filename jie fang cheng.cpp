6e0044ba5d266de25dce02aec44d22e8a695fc37
wangsiqi777 <32270792+wangsiqi777@users.noreply.github.com>
2017-10-10 22:53:35 +0800

    Update jie fang cheng.cpp

--- jie fang cheng.cpp
+++ jie fang cheng.cpp
@@ -2,19 +2,19 @@
 int main()
 {
 float a,b,c,d,e,f,x,y;
-printf("有二元一次方程组：\n");
+printf("鏈変簩鍏冧竴娆℃柟绋嬬粍锛歕n");
 printf("ax+by=c\n");
 printf("dx+ey=f\n");
-printf("请输入相应的a,b,c,d,e,f\n");
+printf("璇疯緭鍏ョ浉搴旂殑a,b,c,d,e,f\n");
 scanf("%f%f%f%f%f%f",&a,&b,&c,&d,&e,&f);
 if(a*e-d*b==0)
 {
-	printf("此方程组无唯一解！\n");
+	printf("姝ゆ柟绋嬬粍鏃犲敮涓�瑙ｏ紒\n");
 }else
 {
 	x=(c*e-b*f)/(a*e-b*d);
 	y=(a*f-c*d)/(a*e-b*d);
-	printf("x=%f,y=%f\n");
+	printf("x=%f,y=%f\n",x,y);
 }
 return 0;
 } 
